f2d02f0454b4a25cf2cfa6b18f50f398926817e3
Jay453 <jeremy.debonville@gmail.com>
2023-04-03 15:57:43 -0400

    test
    hello

---
+++ Untitled.R
@@ -0,0 +1,10 @@
+####GitHub Workshop 2023 ####
+
+library(usethis)
+library(gitcreds)
+gitcreds_get()
+gitcreds_set()
+
+
+usethis::use_git_config(user.name="Jay453", user.email="jeremy.debonville@gmail.com")
+#
